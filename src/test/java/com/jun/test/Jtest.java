c8355058c04c7e59d8f0cd52b5d3d922a4068238
Frank <1164962576@qq.com>
2017-11-06 21:04:09 +0800

    testinit

---
+++ src/test/java/com/jun/test/Jtest.java
@@ -0,0 +1,21 @@
+package com.jun.test;
+
+import org.junit.Test;
+
+/**  
+ * ClassName:Jtest <br/>  
+ * Function:  <br/>  
+ * Date:     Nov 3, 2017 7:11:03 PM <br/>       
+ */
+public class Jtest {
+
+    public static void main(String[] args) {
+        
+
+    }
+    @Test
+    public void f1() {
+        
+    }
+}
+  
